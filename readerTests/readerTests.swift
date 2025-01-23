7a1199d122d157e78d0f36da2ac907ceb0ed3c6c
zero <1490829140@qq.com>
2025-01-23 16:03:37 +0800

    Initial Commit

---
+++ readerTests/readerTests.swift
@@ -0,0 +1,17 @@
+//
+//  readerTests.swift
+//  readerTests
+//
+//  Created by zero on 2025/1/23.
+//
+
+import Testing
+@testable import reader
+
+struct readerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
